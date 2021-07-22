b6a6739372125e60ac18927177eafc0413fd2b0b
Voxny404 <Atomchen92@gmail.com>
2021-07-22 06:54:58 +0200

    adding punch of stuff

--- gameObject/gameExample.js
+++ gameObject/gameExample.js
@@ -4,107 +4,109 @@ document.body.appendChild(div);
 div.innerText = 'GAME EXAMPLE';
 const gameObject = new GameObject();
 
-//creates a rectangle
-gameObject.createGameObjectRect(
-  x = Math.random(),
-  y = Math.random(),
-  width = 100,
-  height = 100,
-  id = 'GreenObject',
-  color = 'green'
-);
+//create the rectangle object
+let greenRect = {
+  x: 0, y: 0, width: 100, height: 100, id: 'GreenObject', color: 'green', overlay: 2,
+};
 
-//creates a rectangle
-gameObject.createGameObjectRect(
-  x = Math.random(),
-  y = Math.random(),
-  width = 100,
-  height = 100,
-  id = 'PurpleObject',
-  color = 'purple'
-);
+let purpleRect = {
+  x: 0, y: 0, width: 100, height: 100, id: 'PurpleObject', color: 'purple', overlay: 1,
+};
+
+let player = {
+  x: window.innerWidth / 2, y: window.innerHeight / 2, width: 30, height: 30, id: 'player', color: 'red', overlay: 0,
+};
+
+//creates the rectangle on the canvas
+gameObject.createGameObjectRect(greenRect);
+
+//creates the rectangle on the canvas
+gameObject.createGameObjectRect(purpleRect);
+
+//creates the rectangle on the canvas
+gameObject.createGameObjectRect(player);
 
 //destroies the object with the id green
 gameObject.destroyGameObject('green');
 
-let testValueGreenW = 0;
-let testValueGreenH = 0;
-let testValuePurpleW = 0;
-let testValuePurpleH = 0;
-let frameW = window.innerWidth;
-let frameH = window.innerHeight;
-
 const animaton = () => {
 
-  //calculate the distance between the object
-  let distancW = Math.floor(testValueGreenW - testValuePurpleW);
-  let distancH = Math.floor(testValueGreenH - testValuePurpleH);
+  //greenRect and purpleRect collision object needs to be updated
+  let collisionObject = {
+    firstX: greenRect.x,
+    firstY: greenRect.y,
+    secondX: purpleRect.x,
+    secondY: purpleRect.y,
+    radius: 100,
+  };
+
+  let collisionPlayerPurple = {
+    firstX: player.x,
+    firstY: player.y,
+    secondX: purpleRect.x,
+    secondY: purpleRect.y,
+    radius: 50,
+  };
+
+  let collisionPlayerGreen = {
+    firstX: player.x,
+    firstY: player.y,
+    secondX: greenRect.x,
+    secondY: greenRect.y,
+    radius: 50,
+  };
+
+  //restore default
+  greenRect.color = 'green';
+  purpleRect.color = 'purple';
+  player.color = 'red';
+
+  //collision behaviour
+  if (gameObject.collision(collisionObject)) {
+    greenRect.color = 'yellow';
+    purpleRect.color = 'blue';
+  }
+
+  if (gameObject.collision(collisionPlayerPurple)) player.color = 'gray';
+  if (gameObject.collision(collisionPlayerGreen)) player.color = 'gray';
+
+  //updates the gameObject
+  gameObject.updateGameObjectRect(greenRect);
+  gameObject.updateGameObjectRect(purpleRect);
+  gameObject.updateGameObjectRect(player);
+
+  //move the object
+  greenRect.x = greenRect.x + 1 * Math.random();
+  greenRect.y = greenRect.y + 5 * Math.random();
+
+  purpleRect.x = purpleRect.x + 5 * Math.random();
+  purpleRect.y = purpleRect.y + 5 * Math.random();
+
+  //detect screen end
+  if (gameObject.worldCollision(greenRect, 100) === 'X') greenRect.x = Math.random();
+  if (gameObject.worldCollision(greenRect, 100) === 'Y') greenRect.y = Math.random();
+
+  if (gameObject.worldCollision(purpleRect) === 'X') purpleRect.x = Math.random();
+  if (gameObject.worldCollision(purpleRect) === 'Y') purpleRect.y = Math.random();
+
+  if (gameObject.worldCollision(player) === 'X') player.x = player.x - 120;
+  if (gameObject.worldCollision(player) === 'Y') player.y = player.y - 120;
+  if (gameObject.worldCollision(player) === '-X') player.x = 1;
+  if (gameObject.worldCollision(player) === '-Y') player.y = 1;
 
   requestAnimationFrame(animaton);
-
-  //simple canvas collision
-  if (distancW <= 100 && distancH <= 100 && distancW >= -100 && distancH >= -100) {
-    gameObject.updateGameObjectRect(
-      x = testValueGreenW,
-      y = testValueGreenH,
-      width = 100,
-      height = 100,
-      id = 'GreenObject',
-      color = 'yellow'
-    );
-    gameObject.updateGameObjectRect(
-      x = testValuePurpleW,
-      y = testValuePurpleH,
-      width = 100,
-      height = 100,
-      id = 'PurpleObject',
-      color = 'blue'
-    );
-  } else {
-    gameObject.updateGameObjectRect(
-      x = testValueGreenW,
-      y = testValueGreenH,
-      width = 100,
-      height = 100,
-      id = 'GreenObject',
-      color = 'green'
-    );
-    gameObject.updateGameObjectRect(
-      x = testValuePurpleW,
-      y = testValuePurpleH,
-      width = 100,
-      height = 100,
-      id = 'PurpleObject',
-      color = 'purple'
-    );
-  }
-
-  //move the object and detect screen end
-  if (testValueGreenW > frameW) {
-    testValueGreenW = Math.random();
-  } else {
-    testValueGreenW = testValueGreenW + 1 * Math.random();
-  }
-
-  if (testValueGreenH > frameH) {
-    testValueGreenH = Math.random();
-  } else {
-    testValueGreenH = testValueGreenH + 5 * Math.random();
-  }
-
-  if (testValuePurpleW > frameW) {
-    testValuePurpleW = Math.random();
-
-  } else {
-    testValuePurpleW = testValuePurpleW + 5 * Math.random();
-  }
-
-  if (testValuePurpleH > frameH) {
-    testValuePurpleH = Math.random();
-  } else {
-    testValuePurpleH = testValuePurpleH + 5 * Math.random();
-  }
-
 };
-console.log(frameW);
+
+//simple event listner for player input
+document.onkeydown = (e) => {
+  //up
+  if (e.keyCode === 87 || e.keyCode === 38) player.y = player.y - 50;
+  //left
+  if (e.keyCode === 65 || e.keyCode === 37) player.x = player.x - 50;
+  //right
+  if (e.keyCode === 68 || e.keyCode === 39) player.x = player.x + 50;
+  //down
+  if (e.keyCode === 83 || e.keyCode === 40) player.y = player.y + 50;
+}
+
 animaton();
--- gameObject/gameObject.js
+++ gameObject/gameObject.js
@@ -3,48 +3,52 @@ class GameObject {
 
   }
 
-  createGameObjectRect(x, y, width, height, id, color) {
+  createGameObjectRect(object) {
+
     document.body.style.overflow = 'hidden';
     let gameObjects = document.createElement('div');
-    gameObjects.setAttribute('id', 'gameObjects');
+    gameObjects.setAttribute('id', 'gameObject');
     document.body.appendChild(gameObjects);
 
     let canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
-    canvas.setAttribute('id', id);
+    canvas.setAttribute('id', object.id);
+    canvas.setAttribute('style', `z-index:${object.overlay}`);
+
     let ctx = canvas.getContext('2d');
 
-    if (!typeof x === 'number') console.log('X: is not a number!');
-    if (!typeof y === 'number') console.log('Y: is not a number!');
-    if (!typeof width === 'number') console.log('WIDHT: is not a number!');
-    if (!typeof height === 'number') console.log('HEIGHT: is not a number!');
-    if (!typeof id === 'string') console.log('ID: is not a string!');
-    if (!typeof color === 'string') console.log('COLOR: is not a string!');
+    if (typeof object.x !== 'number') console.log('X: is not a number!');
+    if (typeof object.y !== 'number') console.log('Y: is not a number!');
+    if (typeof object.width !== 'number') console.log('WIDHT: is not a number!');
+    if (typeof object.height !== 'number') console.log('HEIGHT: is not a number!');
+    if (typeof object.id !== 'string') console.log('ID: is not a string!');
+    if (typeof object.color !== 'string') console.log('COLOR: is not a string!');
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.position = 'fixed';
     canvas.style.top = '0px';
-    
-    ctx.fillStyle = color;
-    ctx.rect(x, y, width, height);
+
+    ctx.fillStyle = object.color;
+    ctx.rect(object.x, object.y, object.width, object.height);
     ctx.fill();
 
     gameObjects.appendChild(canvas);
   }
 
-  updateGameObjectRect(x, y, width, height, id, color) {
-    let canvas = document.getElementById(id);
+  updateGameObjectRect(object) {
 
-    if (!typeof x === 'number') console.log('X: is not a number!');
-    if (!typeof y === 'number') console.log('Y: is not a number!');
-    if (!typeof width === 'number') console.log('WIDHT: is not a number!');
-    if (!typeof height === 'number') console.log('HEIGHT: is not a number!');
-    if (!typeof id === 'string') console.log('ID: is not a string!');
-    if (!typeof color === 'string') console.log('COLOR: is not a string!');
+    let canvas = document.getElementById(object.id);
+
+    if (typeof object.x !== 'number') console.log('X: is not a number!');
+    if (typeof object.y !== 'number') console.log('Y: is not a number!');
+    if (typeof object.width !== 'number') console.log('WIDHT: is not a number!');
+    if (typeof object.height !== 'number') console.log('HEIGHT: is not a number!');
+    if (typeof object.id !== 'string') console.log('ID: is not a string!');
+    if (typeof object.color !== 'string') console.log('COLOR: is not a string!');
 
     if (!canvas) {
-      console.log(`GameObject: ${canvasId} is not existing!`);
+      console.log(`GameObject: ${object.id} is not existing!`);
     } else {
       let ctx = canvas.getContext('2d');
 
@@ -52,18 +56,38 @@ class GameObject {
       canvas.height = window.innerHeight;
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = color;
-      ctx.rect(x, y, width, height);
+      ctx.fillStyle = object.color;
+      ctx.rect(object.x, object.y, object.width, object.height);
       ctx.fill();
     }
   }
+
   destroyGameObject(canvasId) {
     let canvas = document.getElementById(canvasId);
     if (!canvas) {
       console.log(`GameObject: ${canvasId} is not existing!`);
-    } else {
-      canvas.remove();
+    } else { canvas.remove(); }
+  }
+
+  collision(object) {
+    let distanceWidth = Math.floor(object.firstX - object.secondX);
+    let distanceHeight = Math.floor(object.firstY - object.secondY);
+
+    if (distanceWidth <= object.radius &&
+       distanceHeight <= object.radius &&
+       distanceWidth >= -object.radius &&
+       distanceHeight >= -object.radius
+     ) {
+      return true;
     }
-  };
+  }
+
+  worldCollision(object, offset) {
+    if (!offset) offset = 0;
+    if (object.x + offset > window.innerWidth) return 'X';
+    if (((object.x + offset) - window.innerWidth) < -window.innerWidth) return '-X';
+    if (object.y + offset > window.innerHeight) return 'Y';
+    if (((object.y + offset) - window.innerHeight) < -window.innerHeight) return '-Y';
+  }
 
 }
