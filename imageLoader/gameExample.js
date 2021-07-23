984cb49f7b0e91644688217978aeaf1d04e45047
Jessica <Voxny404@gmail.com>
2021-07-23 18:52:21 +0200

    adding tileMap logic

---
+++ imageLoader/gameExample.js
@@ -0,0 +1,75 @@
+let imageLoader = new ImageLoader();
+
+let cloud = {
+  x: 12, y: 3, positionX: 0, positionY: 0,
+  width: 60, height: 60, id: 'cloud', overlay: 1
+}
+
+let objectSky = {
+  x: 12, y: 6, positionX: 0, positionY: 0,
+  width: window.innerWidth, height: window.innerHeight,
+  id: 'sky', overlay: 0
+}
+
+let tree = {
+  treeTopM: {
+    x: 7,
+    y: 6,
+    positionX: window.innerWidth / 2,
+    positionY: window.innerHeight- 50,
+    width: 60,
+    height: 60,
+    id: 'treeM',
+    overlay: 0
+  },
+  treeTopL: {
+    x: 8,
+    y: 6,
+    positionX: window.innerWidth / 2 + 60,
+    positionY: window.innerHeight - 50,
+    width: 60,
+    height: 60,
+    id: 'treeL',
+    overlay: 0
+  },
+  treeTopR: {
+    x: 6,
+    y: 6,
+    positionX: window.innerWidth / 2 -60,
+    positionY: window.innerHeight - 50,
+    width: 60,
+    height: 60,
+    id: 'treeR',
+    overlay: 0
+  },
+}
+
+imageLoader.setTileSize(16);
+imageLoader.loadImage('map.png');
+
+imageLoader.createImageObject('cloud');
+imageLoader.createImageObject('sky');
+
+imageLoader.createImageObject('treeM');
+imageLoader.createImageObject('treeR');
+imageLoader.createImageObject('treeL');
+
+const animation = () => {
+  requestAnimationFrame(animation)
+
+  imageLoader.updateImageObject(tree.treeTopM)
+  imageLoader.updateImageObject(tree.treeTopL)
+  imageLoader.updateImageObject(tree.treeTopR)
+
+  imageLoader.updateImageObject(cloud)
+  imageLoader.updateImageObject(objectSky)
+
+  //simple animation
+  if (cloud.positionX < window.innerWidth) {
+    cloud.positionX = cloud.positionX + 8
+  } else { cloud.positionX = Math.random() }
+
+}
+
+setTimeout(() => imageLoader.destroy('treeM'), 8000)
+animation()
---
+++ imageLoader/imageLoader.js
@@ -0,0 +1,82 @@
+class ImageLoader {
+  constructor() {
+    this.imageMap = null;
+    this.tileSize = null;
+  }
+
+  loadImage(imagePath) {
+    let image = new Image();
+
+    image.src = imagePath;
+    image.onload = () => {
+      this.imageMap = image;
+    }
+  }
+
+  createImageObject(id) {
+    if(!id) console.log('ImageLoader: create must have an id object');
+
+    let div = document.createElement('div');
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.setAttribute('id', id);
+    div.setAttribute('id', 'div_'+id);
+    div.appendChild(canvas);
+    document.body.appendChild(div);
+  }
+
+  setTileSize(size) {
+      this.tileSize = size;
+  }
+
+  updateImageObject(object) {
+
+    if (!object.id) console.log('ImageLoader: NO OBJCET ID SET');
+    if (!object.x) console.log('ImageLoader: NO OBJECT X SET');
+    if (!object.y) console.log('ImageLoader: NO OBJECT Y SET');
+    if (!object.width) console.log('ImageLoader: NO OBJECT WIDTH SET');
+    if (!object.height) console.log('ImageLoader: NO OBJECT HEIGHT SET');
+
+    let canvas = document.getElementById(object.id);
+    if(!canvas) return 
+    let ctx = canvas.getContext('2d');
+    canvas.setAttribute('style', `z-index:${object.overlay}`);
+
+    let tileMap = { width: 0, height: 0 }
+
+    for (var i = 0; i < object.x; i++) {
+      tileMap.width = tileMap.width + 17;
+    }
+
+    for (var i = 0; i < object.y; i++) {
+      tileMap.height = tileMap.height + 17;
+    }
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0px';
+    canvas.style.left = '0px';
+
+    if (this.imageMap) {
+      ctx.drawImage(
+        this.imageMap,
+        tileMap.width,
+        tileMap.height,
+        this.tileSize,
+        this.tileSize,
+        object.positionX,
+        object.positionY,
+        object.width,
+        object.height
+      );
+    }
+  }
+
+  destroy(id) {
+    let div = document.getElementById('div_' + id);
+    if (!div) {
+      console.log(`ImageLoader: ${div} is not existing!`);
+    } else { div.remove(); }
+  }
+}
